c27ab2e551aeaacf513af416fd7895df3b45f63b
Hilcbek <balemayehu07@gmail.com>
2023-10-08 00:29:06 +0300

    added front 2

--- libs/Axios.js
+++ libs/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 
 export let Axios = axios.create({
-    baseURL : 'http://localhost:4000/api',
+    baseURL : 'https://blog-znuc.onrender.com/api',
     withCredentials : true
 })
 
